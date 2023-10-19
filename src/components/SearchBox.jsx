1ddf756f195a805ae1ff8e42c8de5eb6a14e5ebe
Chandni <chandni1141996@gmail.com>
2023-10-19 21:26:44 +0530

    added search box component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,14 @@
+const SearchBox = (props) => {
+  return (
+    <div className="col col-sm-4">
+      <input
+        className="form-control"
+        value={props.searchValue}
+        onChange={(event) => props.setSearchValue(event.target.value)}
+        placeholder="Type to search..."
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
